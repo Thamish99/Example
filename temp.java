a2d0881944402319594c99f06817d8a052b40364
SaadiaJameel <e18147@eng.pdn.ac.lk>
2022-05-26 12:44:47 +0530

    Added a constructor and a method

---
+++ temp.java
@@ -0,0 +1,15 @@
+public class temp{
+
+    int age;
+    String name;
+
+    public temp(int age, String name){
+        this.age = age;
+        this.name = name;
+    }
+
+    public void show(){
+        printf("My name is %s and I am %d years old", name, age);
+    }
+
+}
